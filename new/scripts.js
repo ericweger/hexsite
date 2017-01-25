ad68b2eb5156cbc7517bc8d9c8bc1844e2467a3d
Eric Weger <ericweger@gmail.com>
2017-01-25 15:07:34 -0800

    More styling, initial media breaks, start of filters

--- new/scripts.js
+++ new/scripts.js
@@ -3,14 +3,29 @@ function typeFilter(classname) {
 	for (i=0; i < e.length; i++) {
 		e[i].style.display = e[i].style.display == "none" ? "block" : "none";
 	}
-	rowStarts();
+	
 }
-
 function toggleAll(id) {
 	var e = document.getElementById(id);
 	e.style.display = e.style.display == "none" ? "block" : "none";
 }
-
-function rowStarts() {
+function typeFilter2(classname) {
+	var e = document.getElementsByClassName(classname);
+	e.style.display = e.style.display == "none" ? "block" : "none"
+}
+function clicker(category,id) {
+	var list = document.getElementById(id);
+	var elements = list.getElementsByClassName(category);
+	
+		var e = elements[0];
+		var classes = e.classList;
+		var content = e.innerHTML;
+		var div = document.createElement("DIV");
+		div.innerHTML = content;
+		var newclasses = div.classList;
+		newclasses.add(classes, "hidden");
+		list.insertBefore(div, e);
+	
+	
 	
 }
