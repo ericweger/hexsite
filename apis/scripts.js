665f2c9eabb89ae33ffd4f6de05973687e38c391
Eric Weger <ericweger@gmail.com>
2017-02-08 18:28:54 -0800

    basic api call, filter, display with d3

---
+++ apis/scripts.js
@@ -0,0 +1,33 @@
+//variables
+var population = "B01003_001E";
+var income = "B19013_001E";
+var median_gross_rent = "B25064_001E";
+
+
+function cityCall() {
+	let request = {
+        state : "OR",
+        level : "state",
+        sublevel : true,
+        variables : [
+        	population,
+        	income,
+        	median_gross_rent
+        ],
+        api : "acs5",
+        year : "2013"
+    };
+
+    census.apiRequest(request, callback);
+}
+var callback = function callback(response) {
+
+	let data = response["data"].filter(popFilter);
+	var rows = d3.select("#rawdata").selectAll("p").data(data).text(function(d,i) {return d["NAME"]});
+	rows.enter().append("p").text(function(d,i) {return d["NAME"]});
+	rows.exit().remove();
+};
+
+function popFilter(place) {
+	return place[population] >= 50000;
+}
